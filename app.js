26d4f0d1077aa4b9b6e46cce85a30019c7d93f2f
RizkiRa98 <rizkiramadhan350@gmail.com>
2023-02-07 11:01:00 +0700

    first commit

--- app.js
+++ app.js
@@ -10,18 +10,9 @@ const readline = require('readline').createInterface({
       console.log(`Nama ${name}, nomor ${number}, Email ${email}`);
   readline.close();
 
-//buat folder
-//cek folder sudah ada atau tidak, jika tidak buat folder
-if (!fs.existsSync('data')){
-  fs.mkdirSync('data');
-}
-//cek file sudah ada atau tidak, jika tidak buat file
-if(!fs.existsSync('./data/contacts.json')){
-  fs.writeFileSync('./data/contacts.json', '[]', 'utf-8');
-}
 
 //baca terlebih dahulu array yang ada di JSON
-    fs.readFile('./data/contacts.json', 'utf-8', function readFileCallBack(err, data){
+    fs.readFile('contacts.json', 'utf-8', function readFileCallBack(err, data){
       if(err){
         console.log(err); //cek error
       }else{
@@ -29,7 +20,7 @@ if(!fs.existsSync('./data/contacts.json')){
         arr.push({name: name, number: number, email: email}); //Push array ke JSON
         const json = JSON.stringify(arr);
         //write array ke JSON
-        fs.writeFile('./data/contacts.json', json, function callback(err){
+        fs.writeFile('contacts.json', json, function callback(err){
           if(err){
             console.log(err);
           }
